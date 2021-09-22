c0be38aa5640e643f4d1d0c16656e7ccbec0d3ae
Yesica <yesibarros95@gmail.com>
2021-09-22 17:27:36 +0200

    mejoras front

--- src/components/SecretIdForm.jsx
+++ src/components/SecretIdForm.jsx
@@ -1,15 +1,22 @@
-import React, { useEffect, useState } from "react";
-import { View, Text, StyleSheet } from "react-native";
-import { TextInput, Button } from "react-native-paper";
-import { BarCodeScanner } from "expo-barcode-scanner";
-import { retrieveData } from "../services/syncdata";
-
+import React, { useEffect, useState } from 'react';
+import { View, Text, StyleSheet } from 'react-native';
+import { TextInput, Button, Avatar } from 'react-native-paper';
+import { BarCodeScanner } from 'expo-barcode-scanner';
+import { retrieveData } from '../services/syncdata';
+import DateTimePicker from '@react-native-community/datetimepicker';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 const SecretIdForm = ({ setPatientData }) => {
-  const [secretId, setSecretId] = useState("");
+  const [secretId, setSecretId] = useState('');
+  const [name, setName] = useState('');
+  const [lastName, setLastName] = useState('');
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [scanning, setScanning] = useState(false);
-  const [error, setError] = useState("");
+  const [error, setError] = useState('');
+  const [date, setDate] = useState(new Date());
 
   const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
@@ -17,48 +24,46 @@ const SecretIdForm = ({ setPatientData }) => {
     retrieveData(data, setPatientData);
   };
 
+  const onChange = (event, selectedDate) => {
+    const currentDate = selectedDate || date;
+    setDate(currentDate);
+    console.log('date', date);
+  };
   const handleScanBarcode = async () => {
     await (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === "granted");
+      setHasPermission(status === 'granted');
     })();
     if (hasPermission) setScanning(true);
   };
 
   return (
     <View>
+      <Text
+        style={{
+          textAlign: 'center',
+          fontSize: 20,
+        }}
+      >
+        Introduce one of the following items
+      </Text>
       <View style={styles.container}>
-        <Text style={{ textAlign: "center" }}>
-          Introduce your Secret Id or SCAN QR Code
-        </Text>
         <TextInput
           placeholder="Secret Id"
           value={secretId}
           mode="outlined"
           onChangeText={(text) => setSecretId(text)}
+          style={{ marginTop: hp('3%') }}
         />
-        <Button
-          style={styles.button}
-          disabled={!secretId}
-          mode="outlined"
-          onPress={() => {
-            retrieveData(secretId, setPatientData);
+        <Avatar.Icon
+          color="white"
+          icon="chevron-down"
+          style={{
+            position: 'relative',
+            left: hp('17%'),
+            marginTop: wp('3%'),
           }}
-        >
-          Submit
-        </Button>
-        <Button
-          style={styles.button}
-          disabled={!secretId}
-          mode="outlined"
-          color="red"
-          onPress={() => {
-            setScanned(false);
-            setSecretId("");
-          }}
-        >
-          Reset
-        </Button>
+        />
         {!scanning && (
           <Button
             style={styles.button}
@@ -69,6 +74,96 @@ const SecretIdForm = ({ setPatientData }) => {
             SCAN QR CODE
           </Button>
         )}
+        <Avatar.Icon
+          color="white"
+          icon="chevron-down"
+          style={{
+            position: 'relative',
+            left: hp('17%'),
+            marginTop: hp('5%'),
+          }}
+        />
+        <View
+          style={{
+            display: 'flex',
+            flexDirection: 'column',
+            justifyContent: 'space-between',
+          }}
+        >
+          <Text
+            style={{
+              display: 'flex',
+              textAlign: 'center',
+              marginTop: hp('3%'),
+            }}
+          >
+            Date Birth
+          </Text>
+
+          <DateTimePicker
+            testID="dateTimePicker"
+            value={date}
+            is24Hour={true}
+            display="default"
+            dateFormat="day month year"
+            timeZoneOffsetInMinutes={60}
+            timeZoneOffsetInSeconds={3600}
+            onChange={onChange}
+            style={{
+              display: 'flex',
+              flexDirection: 'column',
+              position: 'relative',
+              left: wp('30%'),
+              width: hp('100%'),
+            }}
+          />
+          <TextInput
+            placeholder="Name"
+            value={name}
+            mode="outlined"
+            onChangeText={(text) => setName(text)}
+          />
+          <TextInput
+            placeholder="Last Name"
+            value={lastName}
+            mode="outlined"
+            onChangeText={(text) => setLastName(text)}
+          />
+        </View>
+        <View
+          style={{
+            zIndex: '1000',
+            display: 'flex',
+            flexDirection: 'row',
+            justifyContent: 'space-evenly',
+            position: 'fixed',
+            top: hp('5%'),
+            size: hp('10%'),
+          }}
+        >
+          <Button
+            style={styles.button}
+            disabled={!secretId}
+            mode="outlined"
+            color="red"
+            onPress={() => {
+              setScanned(false);
+              setSecretId('');
+            }}
+          >
+            Reset
+          </Button>
+          <Button
+            style={styles.button}
+            disabled={!secretId}
+            mode="outlined"
+            onPress={() => {
+              retrieveData(secretId, setPatientData);
+            }}
+          >
+            Submit
+          </Button>
+        </View>
       </View>
       {scanning && (
         <View style={styles.qrContainer}>
@@ -85,18 +180,19 @@ const SecretIdForm = ({ setPatientData }) => {
 
 const styles = StyleSheet.create({
   qrContainer: {
-    position: "absolute",
+    position: 'absolute',
     top: 0,
-    width: "100%",
-    height: "100%",
+    width: '100%',
+    height: '100%',
   },
   container: {
-    backgroundColor: "white",
-    height: "100%",
+    backgroundColor: 'white',
+    height: hp('100%'),
     paddingHorizontal: 20,
   },
   button: {
     marginVertical: 15,
+    marginTop: hp('5%'),
   },
 });
 
