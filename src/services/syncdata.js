56c33d31b0ce7508b8865ef5cb92ae66650316ed
Alfonso Tienda <afoone@hotmail.com>
2021-09-27 22:16:48 +0200

    production urls

--- src/services/syncdata.js
+++ src/services/syncdata.js
@@ -2,7 +2,7 @@ import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export const retrieveData = async (patientId, callback,date, name, lastName) => {
-  let link = !patientId ? `http://draman.iprocuratio.com/api/patients/checkPatient?firstName=${name}&lastName=${lastName}&birthDate=${date}` : `https://draman.iprocuratio.com/api/patients/${patientId}`
+  let link = !patientId ? `https://covid19.crvs.gm/api/patients/checkPatient?firstName=${name}&lastName=${lastName}&birthDate=${date}` : `https://covid19.crvs.gm/api/patients/${patientId}`
   const res = await axios
     .get(link)
     .catch((err) => {
