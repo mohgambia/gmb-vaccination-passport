1e8de6d4cf92cf6bbe3427ea0d23e5298fd4beeb
Alfonso Tienda <afoone@hotmail.com>
2021-09-28 21:05:36 +0200

    doest look good on iphone 12

--- src/components/SecretIdForm.jsx
+++ src/components/SecretIdForm.jsx
@@ -1,21 +1,21 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput, Button, Avatar } from 'react-native-paper';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { retrieveData } from '../services/syncdata';
-import DateTimePicker from '@react-native-community/datetimepicker';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TextInput, Button, Avatar } from "react-native-paper";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { retrieveData } from "../services/syncdata";
+import DateTimePicker from "@react-native-community/datetimepicker";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+} from "react-native-responsive-screen";
 const SecretIdForm = ({ setPatientData }) => {
-  const [secretId, setSecretId] = useState('');
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
+  const [secretId, setSecretId] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [scanning, setScanning] = useState(false);
-  const [error, setError] = useState('');
+  const [error, setError] = useState("");
   const [date, setDate] = useState(new Date());
 
   const handleBarCodeScanned = async ({ type, data }) => {
@@ -27,36 +27,37 @@ const SecretIdForm = ({ setPatientData }) => {
   const onChange = (event, selectedDate) => {
     const currentDate = selectedDate || date;
     setDate(currentDate);
-    console.log('date', date);
+    console.log("date", date);
   };
   const handleScanBarcode = async () => {
     await (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
     if (hasPermission) setScanning(true);
   };
   function formatDate(date) {
     var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
       year = d.getFullYear();
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
 
-    return [year, month, day].join('-');
+    return [year, month, day].join("-");
   }
   let dateFormat = formatDate(date);
   return (
     <View>
       <Text
         style={{
-          textAlign: 'center',
-          fontSize: 20,
+          textAlign: "center",
+          fontSize: 13,
+          backgroundColor: "white"
         }}
       >
-        Introduce one of the following items
+        Introduce one of the following items:
       </Text>
       <View style={styles.container}>
         <TextInput
@@ -64,15 +65,16 @@ const SecretIdForm = ({ setPatientData }) => {
           value={secretId}
           mode="outlined"
           onChangeText={(text) => setSecretId(text)}
-          style={{ marginTop: hp('3%') }}
+          style={{ marginTop: hp("3%") }}
         />
         <Avatar.Icon
           color="white"
           icon="chevron-down"
+          size={30}
           style={{
-            position: 'relative',
-            left: hp('17%'),
-            marginTop: wp('3%'),
+            position: "relative",
+            left: hp("17%"),
+            marginTop: wp("2%"),
           }}
         />
         {!scanning && (
@@ -88,46 +90,55 @@ const SecretIdForm = ({ setPatientData }) => {
         <Avatar.Icon
           color="white"
           icon="chevron-down"
+          size={30}
           style={{
-            position: 'relative',
-            left: hp('17%'),
-            marginTop: hp('5%'),
+            position: "relative",
+            left: hp("17%"),
+            marginTop: hp("2%"),
           }}
         />
         <View
           style={{
-            display: 'flex',
-            flexDirection: 'column',
-            justifyContent: 'space-between',
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "space-between",
           }}
         >
-          <Text
+          <View
             style={{
-              display: 'flex',
-              textAlign: 'center',
-              marginTop: hp('3%'),
+              display: "flex",
+              flexDirection: "row"
             }}
           >
-            Date Birth
-          </Text>
+            <Text
+              style={{
+                display: "flex",
+                textAlign: "center",
+                marginTop: hp("3%"),
+              }}
+            >
+              Date of Birth
+            </Text>
 
-          <DateTimePicker
-            testID="dateTimePicker"
-            value={date}
-            is24Hour={true}
-            display="default"
-            dateFormat="day month year"
-            timeZoneOffsetInMinutes={60}
-            timeZoneOffsetInSeconds={3600}
-            onChange={onChange}
-            style={{
-              display: 'flex',
-              flexDirection: 'column',
-              position: 'relative',
-              left: wp('30%'),
-              width: hp('100%'),
-            }}
-          />
+            <DateTimePicker
+              testID="dateTimePicker"
+              value={date}
+              is24Hour={true}
+              display="default"
+              dateFormat="day month year"
+              timeZoneOffsetInMinutes={60}
+              timeZoneOffsetInSeconds={3600}
+              onChange={onChange}
+              style={{
+                display: "flex",
+                flexDirection: "column",
+                position: "relative",
+                left: wp("30%"),
+                width: hp("100%"),
+              }}
+            />
+          </View>
+
           <TextInput
             placeholder="Name"
             value={name}
@@ -143,13 +154,13 @@ const SecretIdForm = ({ setPatientData }) => {
         </View>
         <View
           style={{
-            zIndex: '1000',
-            display: 'flex',
-            flexDirection: 'row',
-            justifyContent: 'space-evenly',
-            position: 'fixed',
-            top: hp('5%'),
-            size: hp('10%'),
+            zIndex: "1000",
+            display: "flex",
+            flexDirection: "row",
+            justifyContent: "space-evenly",
+            position: "fixed",
+            top: hp("5%"),
+            size: hp("10%"),
           }}
         >
           <Button
@@ -159,7 +170,7 @@ const SecretIdForm = ({ setPatientData }) => {
             color="red"
             onPress={() => {
               setScanned(false);
-              setSecretId('');
+              setSecretId("");
             }}
           >
             Reset
@@ -197,19 +208,19 @@ const SecretIdForm = ({ setPatientData }) => {
 
 const styles = StyleSheet.create({
   qrContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   container: {
-    backgroundColor: 'white',
-    height: hp('100%'),
+    backgroundColor: "white",
+    height: hp("100%"),
     paddingHorizontal: 20,
   },
   button: {
     marginVertical: 15,
-    marginTop: hp('5%'),
+    marginTop: hp("5%"),
   },
 });
 
