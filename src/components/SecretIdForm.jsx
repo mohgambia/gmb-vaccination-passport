d96063cdccb75e906ced97540b7b02c62102251e
Alfonso Tienda <afoone@hotmail.com>
2021-10-01 12:42:06 +0200

    v1

--- src/components/SecretIdForm.jsx
+++ src/components/SecretIdForm.jsx
@@ -1,91 +1,89 @@
-import React, { useEffect, useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
-import { TextInput, Button, Avatar } from 'react-native-paper';
-import { BarCodeScanner } from 'expo-barcode-scanner';
-import { retrieveData } from '../services/syncdata';
-import { Formik } from 'formik';
-import * as yup from 'yup';
+import React, { useEffect, useState } from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { TextInput, Button, Avatar } from "react-native-paper";
+import { BarCodeScanner } from "expo-barcode-scanner";
+import { retrieveData } from "../services/syncdata";
+import { Formik } from "formik";
+import * as yup from "yup";
 import {
   widthPercentageToDP as wp,
   heightPercentageToDP as hp,
-} from 'react-native-responsive-screen';
+} from "react-native-responsive-screen";
 
 let validationSchemaData = yup.object().shape({
   dayData: yup.number().min(1).max(31),
   monthData: yup.number().min(1).max(12),
   yearData: yup.number().min(1900).max(2021),
+  secretId: yup.mixed(),
 });
 
 const SecretIdForm = ({ setPatientData }) => {
-  const [secretId, setSecretId] = useState('');
-  const [name, setName] = useState('');
-  const [lastName, setLastName] = useState('');
-  const [dayData, setDay] = useState('');
-  const [monthData, setMonth] = useState('');
-  const [yearData, setYear] = useState('');
+  const [secretId, setSecretId] = useState("");
+  const [name, setName] = useState("");
+  const [lastName, setLastName] = useState("");
+
   const [hasPermission, setHasPermission] = useState(null);
   const [scanned, setScanned] = useState(false);
   const [scanning, setScanning] = useState(false);
-  const [error, setError] = useState('');
-  const [date, setDate] = useState(new Date());
-  console.log('dayData', dayData);
-  console.log('monthData', monthData);
-  console.log('yearData', yearData);
+  const [error, setError] = useState("");
+  const [date, setDate] = useState("");
+
   const handleBarCodeScanned = async ({ type, data }) => {
     setScanned(true);
     setSecretId(data);
     retrieveData(data, setPatientData);
   };
 
-  const onChange = (event, selectedDate) => {
-    const currentDate = selectedDate || date;
-    setDate(currentDate);
-    console.log('date', date);
-  };
   const handleScanBarcode = async () => {
     await (async () => {
       const { status } = await BarCodeScanner.requestPermissionsAsync();
-      setHasPermission(status === 'granted');
+      setHasPermission(status === "granted");
     })();
     if (hasPermission) setScanning(true);
   };
   function formatDate(date) {
     var d = new Date(date),
-      month = '' + (d.getMonth() + 1),
-      day = '' + d.getDate(),
+      month = "" + (d.getMonth() + 1),
+      day = "" + d.getDate(),
       year = d.getFullYear();
 
-    if (month.length < 2) month = '0' + month;
-    if (day.length < 2) day = '0' + day;
+    if (month.length < 2) month = "0" + month;
+    if (day.length < 2) day = "0" + day;
 
-    return [year, month, day].join('-');
+    return [year, month, day].join("-");
   }
 
-  let dateFormat = `${yearData}-${monthData}-${dayData}`;
   //necesito poner aca values.yearData cada uno pero no esta el valor
 
   return (
     <Formik
-      initialValues={{ dayData: '', monthData: '', yearData: '' }}
+      initialValues={{
+        secretId: "",
+        dayData: "",
+        monthData: "",
+        yearData: "",
+        firstName: "",
+        lastName: "",
+      }}
       validateOnMount={true}
-      onSubmit={(values) => console.log(values)}
+      onSubmit={(values) => {
+        retrieveData(
+          values.secretId,
+          setPatientData, setError,
+          `${values.yearData}-${values.monthData.padStart(2,"0")}-${values.dayData}`,
+          values.firstName,
+          values.lastName
+        );
+      }}
       validationSchema={validationSchemaData}
     >
-      {({
-        handleChange,
-        handleBlur,
-        handleSubmit,
-        values,
-        touched,
-        errors,
-        isValid,
-      }) => (
+      {({ handleChange, handleBlur, handleSubmit, values, errors }) => (
         <View>
           <Text
             style={{
-              textAlign: 'center',
+              textAlign: "center",
               fontSize: 13,
-              backgroundColor: 'white',
+              backgroundColor: "white",
             }}
           >
             Introduce one of the following items:
@@ -93,19 +91,20 @@ const SecretIdForm = ({ setPatientData }) => {
           <View style={styles.container}>
             <TextInput
               placeholder="Secret Id"
-              value={secretId}
+              onChangeText={handleChange("secretId")}
+              onBlur={handleBlur("secretId")}
+              value={values.secretId}
               mode="outlined"
-              onChangeText={(text) => setSecretId(text)}
-              style={{ marginTop: hp('3%') }}
+              style={{ marginTop: hp("3%") }}
             />
             <Avatar.Icon
               color="white"
               icon="chevron-down"
               size={30}
               style={{
-                position: 'relative',
-                left: hp('20%'),
-                marginTop: wp('2%'),
+                position: "relative",
+                left: hp("20%"),
+                marginTop: wp("2%"),
               }}
             />
             {!scanning && (
@@ -123,39 +122,39 @@ const SecretIdForm = ({ setPatientData }) => {
               icon="chevron-down"
               size={30}
               style={{
-                position: 'relative',
-                left: hp('20%'),
-                marginTop: hp('2%'),
+                position: "relative",
+                left: hp("20%"),
+                marginTop: hp("2%"),
               }}
             />
             <View
               style={{
-                display: 'flex',
-                flexDirection: 'column',
-                justifyContent: 'space-between',
+                display: "flex",
+                flexDirection: "column",
+                justifyContent: "space-between",
               }}
             >
               <View>
                 <Text
                   style={{
-                    display: 'flex',
-                    textAlign: 'center',
-                    marginTop: hp('3%'),
+                    display: "flex",
+                    textAlign: "center",
+                    marginTop: hp("3%"),
                   }}
                 >
                   Date of Birth
                 </Text>
                 <View
                   style={{
-                    display: 'flex',
-                    flexDirection: 'row',
-                    justifyContent: 'space-between',
+                    display: "flex",
+                    flexDirection: "row",
+                    justifyContent: "space-between",
                   }}
                 >
-                  {console.log('values', values)}
+                  {console.log("values", values)}
                   <TextInput
-                    onChangeText={handleChange('dayData')}
-                    onBlur={handleBlur('dayData')}
+                    onChangeText={handleChange("dayData")}
+                    onBlur={handleBlur("dayData")}
                     value={values.dayData}
                     placeholder="Day"
                     mode="outlined"
@@ -174,8 +173,8 @@ const SecretIdForm = ({ setPatientData }) => {
                     </Text>
                   )} */}
                   <TextInput
-                    onChangeText={handleChange('monthData')}
-                    onBlur={handleBlur('monthData')}
+                    onChangeText={handleChange("monthData")}
+                    onBlur={handleBlur("monthData")}
                     value={values.monthData}
                     placeholder="Month"
                     mode="outlined"
@@ -184,21 +183,21 @@ const SecretIdForm = ({ setPatientData }) => {
                     // }
                   />
                   <TextInput
-                    onChangeText={handleChange('yearData')}
-                    onBlur={handleBlur('yearData')}
+                    onChangeText={handleChange("yearData")}
+                    onBlur={handleBlur("yearData")}
                     value={values.yearData}
                     placeholder="Year"
                     mode="outlined"
                     // onChangeText={(text) => setYear(text)}
                   />
                 </View>
-                {console.log('errors', errors)}
+                {console.log("errors", errors)}
                 {errors.dayData && (
                   <Text
                     style={{
                       fontSize: 14,
-                      color: 'red',
-                      fontWeight: 'bold',
+                      color: "red",
+                      fontWeight: "bold",
                       marginTop: 5,
                     }}
                   >
@@ -209,57 +208,59 @@ const SecretIdForm = ({ setPatientData }) => {
 
               <TextInput
                 placeholder="Name"
-                value={name}
                 mode="outlined"
-                onChangeText={(text) => setName(text)}
+                onChangeText={handleChange("firstName")}
+                onBlur={handleBlur("firstName")}
+                value={values.firstName}
               />
               <TextInput
                 placeholder="Last Name"
-                value={lastName}
                 mode="outlined"
-                onChangeText={(text) => setLastName(text)}
+                onChangeText={handleChange("lastName")}
+                onBlur={handleBlur("lastName")}
+                value={values.lastName}
               />
             </View>
             <View
               style={{
                 zIndex: 1000,
-                display: 'flex',
-                flexDirection: 'row',
-                justifyContent: 'space-evenly',
+                display: "flex",
+                flexDirection: "row",
+                justifyContent: "space-evenly",
                 // position: "fixed",
-                top: hp('5%'),
-                size: hp('10%'),
+                top: hp("5%"),
+                size: hp("10%"),
               }}
             >
               <Button
                 style={styles.button}
-                disabled={
-                  !secretId &&
-                  !name &&
-                  !lastName &&
-                  !dayData &&
-                  !monthData &&
-                  !yearData
-                }
+                // disabled={
+                //   !secretId &&
+                //   !name &&
+                //   !lastName &&
+                //   !dayData &&
+                //   !monthData &&
+                //   !yearData
+                // }
                 mode="outlined"
                 color="red"
                 onPress={() => {
                   setScanned(false);
-                  setSecretId('');
+                  setSecretId("");
                 }}
               >
                 Reset
               </Button>
               <Button
                 style={styles.button}
-                disabled={
-                  !secretId &&
-                  !name &&
-                  !lastName &&
-                  !dayData &&
-                  !monthData &&
-                  !yearData
-                }
+                // disabled={
+                //   !secretId &&
+                //   !name &&
+                //   !lastName &&
+                //   !dayData &&
+                //   !monthData &&
+                //   !yearData
+                // }
                 mode="outlined"
                 type="submit"
                 onPress={() => {
@@ -274,6 +275,7 @@ const SecretIdForm = ({ setPatientData }) => {
               >
                 Submit
               </Button>
+              <Button onPress={handleSubmit}>SUBMIT NEW</Button>
             </View>
           </View>
           {scanning && (
@@ -293,19 +295,19 @@ const SecretIdForm = ({ setPatientData }) => {
 
 const styles = StyleSheet.create({
   qrContainer: {
-    position: 'absolute',
+    position: "absolute",
     top: 0,
-    width: '100%',
-    height: '100%',
+    width: "100%",
+    height: "100%",
   },
   container: {
-    backgroundColor: 'white',
-    height: hp('100%'),
+    backgroundColor: "white",
+    height: hp("100%"),
     paddingHorizontal: 20,
   },
   button: {
     marginVertical: 15,
-    marginTop: hp('5%'),
+    marginTop: hp("5%"),
   },
 });
 
--- src/services/syncdata.js
+++ src/services/syncdata.js
@@ -1,15 +1,24 @@
 import axios from "axios";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 
-export const retrieveData = async (patientId, callback,date, name, lastName) => {
-  let link = !patientId ? `https://covid19.crvs.gm/api/patients/checkPatient?firstName=${name.toUpperCase()}&lastName=${lastName.toUpperCase()}&birthDate=${date}` : `https://covid19.crvs.gm/api/patients/${patientId}`
+export const retrieveData = async (
+  patientId,
+  callback,
+  setError,
+  date,
+  name,
+  lastName
+) => {
+  console.log("retrieve data", patientId, date, name, lastName);
 
-  const res = await axios
-    .get(link)
-    .catch((err) => {
-      console.error(err);
-      setError("There was an error: " + err);
-    });
+  let link = !patientId
+    ? `https://covid19.crvs.gm/api/patients/checkPatient?firstName=${name.toUpperCase()}&lastName=${lastName.toUpperCase()}&birthDate=${date}`
+    : `https://covid19.crvs.gm/api/patients/${patientId}`;
+
+  const res = await axios.get(link).catch((err) => {
+    console.error(err);
+    setError("There was an error: " + err);
+  });
   console.log("retrieved data", res.data);
   await AsyncStorage.setItem("secretId", patientId);
   await AsyncStorage.setItem("data", res ? JSON.stringify(res.data) : null);
